fc4d8be2f085489cf4a63f8222e7f9adf676f9ad
Alymbekov <maxim.makarov.1997@mail.ru>
2020-11-13 21:47:03 +0600

    Add Full Registation and Permissions logic

---
+++ apps/users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from apps.users.views import (
+    UserRegistrationView, IndexPageView,
+    CabinetView)
+
+app_name = "users"
+
+urlpatterns = [
+    path('accounts/register/', UserRegistrationView.as_view(), name="register"),
+    path('accounts/cabinet/', CabinetView.as_view(), name='profile'),
+    path('', IndexPageView.as_view(), name='index'),
+
+]
+
+
